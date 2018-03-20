88ca7972cbe8997f0d89f63a80e92fecf03e9df3
munagekar <avm.abhishek@gmail.com>
2018-03-20 17:39:41 +0530

    Version1

--- bencher.py
+++ bencher.py
@@ -1,12 +1,16 @@
+# coding=utf-8
 '''
 bencher.py : Python 2
 Author : Abhishek Munagekar
 
+
+
+Call Using
+python bencher.py -ifols 10 net10 20 net20 25 net25 35 net35 45 net45 -itypes cqi10 net10 cqi20 net20 cqi25 net25 cqi35 net35 cqi45 net45 -rfol label --comps 0vs1 2vs3 4vs5 6vs7 8vs9
 Comparisons
 vmaf
 psnr
 ssim
-ms_ssim
 phone_model
 
 Dependencies
@@ -18,6 +22,8 @@ Input Folder : Folder Containing the files given as input to the network
 Output Folder : Fodler Contina the files given as output by the network
 Reference Folder : Folder Containing the files to be used as reference
 '''
+
+
 import argparse
 import os
 import core
@@ -25,22 +31,18 @@ from multiprocessing import Pool
 from numpy import median, mean
 
 
-# Helper Functions
-def build_parser():
-    parser = argparse.ArgumentParser(description='Fast Folder Benchmarking')
-    parser.add_argument('-if', type=str, nargs='+',
-                        help='Folders Containing Input Files')
-    parser.add_argument('-r', type=str,
-                        help='Folder Containing the Reference Files')
-    parser.add_argument('-itype', type=str, nargs='+',
-                        help='Type of Images in Infolder')
-    parser.add_argument('--statfile', type=str, default="benchmark.txt",
-                        help='Name/Location for Benchmark file')
-    return parser
-
+def magprint(num):
+    if num == 0:
+        return(' ' + str(num))
+    elif num > 0:
+        return('+' + str(num))
+    else:
+        return('-' + str(num))
 
 # Returns the median, average, min, max of a list
 # Returns a dictionary for easy access
+
+
 def liststat(l):
     stat = {}
     stat['medain'] = median(l)
@@ -48,6 +50,23 @@ def liststat(l):
     stat['max'] = max(l)
     stat['min'] = min(l)
     return stat
+
+
+def diff(stat1, stat2):
+    diffstat = {}
+    psnrdiff = []
+    vmafdiff = []
+    vmafpdiff = []
+    ssimdiff = []
+    for x, y in zip(stat1, stat2):
+        psnrdiff.append(x['psnr'] - y['psnr'])
+        vmafdiff.append(x['vmaf'] - y['vmaf'])
+        vmafpdiff.append(x['vmafp'] - y['vmafp'])
+        ssimdiff.append(x['ssim'] - y['ssim'])
+    diffstat['psnr'] = liststat(psnrdiff)
+    diffstat['vmaf'] = liststat(vmafdiff)
+    diffstat['vmafp'] = liststat(vmafpdiff)
+    diffstat['ssim'] = liststat(ssimdiff)
 
 
 # Helper Function for Pool Jobs
@@ -66,88 +85,100 @@ def statcompare(i, ref):
     return params
 
 
+# A Modification of os.listdir
+# Gives the Path along with the file names
+def pathdir(dir):
+    return [os.path.join(dir, x) for x in os.listdir(dir)]
+
+
+# TODO : Make a proper check for extension
 # Remove thumbnails as well other temp files
-def cleanpnglist(ilist):
+def cleanlist(ilist, ext):
     cleanedlist = []
     for item in ilist:
-        if 'png' in item:
+        if ext in item:
             cleanedlist.append(item)
     return cleanedlist
+
+
+# Helper Functions
+def build_parser():
+    parser = argparse.ArgumentParser(description='Fast Folder Benchmarking')
+    parser.add_argument('-ifols', type=str, nargs='+',
+                        help='Folders Containing Input Files')
+    parser.add_argument('-rfol', type=str,
+                        help='Folder Containing the Reference Files')
+    parser.add_argument('-itypes', type=str, nargs='+',
+                        help='Type of Images in Infolder')
+    parser.add_argument('--statfile', type=str, default="benchmark.txt",
+                        help='Name/Location for Benchmark file')
+    parser.add_argument('--comps', type=str, nargs='+',
+                        help='1vs2 2vs3 4vs5 etc')
+    return parser
 
 
 parser = build_parser()
 args = parser.parse_args()
 
-IN = args.i
-REF = args.r
-OUT = args.o
-INLABEL = args.itype
-OUTLABEL = args.otype
+# Multiple Inputs for the same reference
+
+INS = args.ifols
+REF = args.rfol
+INLABELS = args.itypes
 BFILE = args.statfile
+COMPS = args.comps
 
-assert (os.path.exists(IN) and
-        os.path.exists(REF)and
-        os.path.exists(OUT)), "Invlaid Folder Paths"
 
-infiles = cleanpnglist(os.listdir(IN))
-infiles = map(lambda x: os.path.join(IN, x), infiles)
-outfiles = cleanpnglist(os.listdir(OUT))
-outfiles = map(lambda x: os.path.join(OUT, x), outfiles)
-reffiles = cleanpnglist(os.listdir(REF))
-reffiles = map(lambda x: os.path.join(REF, x), reffiles)
+# Existence Check
+for folder in INS:
+    assert(os.path.exists(folder)), folder + " doesn't exist"
+assert(os.path.exists(REF)), REF + "doesn't exist"
+
+
+infiles = [cleanlist(pathdir(x), 'png') for x in INS]
+reffiles = cleanlist(pathdir(REF), 'png')
+
 
 # Inplace Sort
-infiles.sort()
-outfiles.sort()
+for x in infiles:
+    x.sort()
 reffiles.sort()
 
-
-assert(len(infiles) == len(outfiles) and
-       len(outfiles) == len(reffiles)), "Num files Mismatch"
+# Length Check
+for x in infiles:
+    assert(len(x) == len(reffiles)), "File Number Mismatch Occured"
 
 pool = Pool()
-istats = pool.map(statcomparehelper, zip(infiles, reffiles))
-ostats = pool.map(statcomparehelper, zip(outfiles, reffiles))
-
+istats = []
+print('Parallel Processing : Using All Cores')
+print('Please be Patient....')
+for i, x in enumerate(infiles):
+    istats.append(pool.map(statcomparehelper, zip(x, reffiles)))
+    # TODO : Add a Prog Bar Call Here
 
 fp = open(BFILE, 'w')
 fp.write('Type of File,File Location,Reference Location,' +
          'PSNR,SSIM,VMAF,VMAF Phone\n')
 
+# For Istatfol
+for item in range(len(reffiles)):
+    for i, istatfol in enumerate(istats):
+        fp.write(INLABELS[i] + ',')
+        fp.write(istatfol[item]['ifile'] + ',')
+        fp.write(istatfol[item]['rfile'] + ',')
+        fp.write(istatfol[item]['psnr'] + ',')
+        fp.write(istatfol[item]['ssim'] + ',')
+        fp.write(istatfol[item]['vmaf'] + ',')
+        fp.write(istatfol[item]['vmafp'])
+        fp.write('\n')
 
-assert (len(istats) == len(ostats)), "Possible Crash in ffmpeg"
 
-gpsnr = 0
-gssim = 0
-gvmaf = 0
-gvmafp = 0
-
-
-for istat, ostat in zip(istats, ostats):
-    fp.write(INLABEL + ',')
-    fp.write(istat['ifile'] + ',')
-    fp.write(istat['rfile'] + ',')
-    fp.write(istat['psnr'] + ',')
-    fp.write(istat['ssim'] + ',')
-    fp.write(istat['vmaf'] + ',')
-    fp.write(istat['vmafp'])
-    fp.write('\n')
-
-    fp.write(OUTLABEL + ',')
-    fp.write(ostat['ifile'] + ',')
-    fp.write(ostat['rfile'] + ',')
-    fp.write(ostat['psnr'] + ',')
-    fp.write(ostat['ssim'] + ',')
-    fp.write(ostat['vmaf'] + ',')
-    fp.write(ostat['vmafp'])
-    fp.write('\n')
-
-    gpsnr += (float(ostat['psnr']) - float(istat['psnr']))
-    gssim += (float(ostat['ssim']) - float(istat['ssim']))
-    gvmaf += (float(ostat['vmaf']) - float(istat['vmaf']))
-    gvmafp += (float(ostat['vmafp']) - float(istat['vmafp']))
-
-print "Gain PSNR:", gpsnr / len(istats)
-print "Gain SSIM:", gssim / len(istats)
-print "Gain VMAF:", gvmaf / len(istats)
-print "Gain VMAFP:", gvmafp / len(istats)
+for i in range(len(COMPS)):
+    fol1, fol2 = COMPS[i].split('vs')
+    n1 = int(fol1)
+    n2 = int(fol2)
+    print('Gains', INLABELS[n1], 'vs', INLABELS[n2], '\n')
+    diffstat = diff(istats[n1], istats[n2])
+    metrics = ['psnr', 'ssim', 'vmaf', 'vmafp']
+    for metric in metrics:
+        print(metric, ':', magprint(diffstat[metrics]), '\n')
